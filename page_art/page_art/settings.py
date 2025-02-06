8f2b4738c20c771091fb5ce1b5ae24d7edfdb83f
Elvis Rodrigo  Condori Vasquez <ec7902812@gmail.com>
2025-02-06 01:53:45 -0400

    finally

--- page_art/page_art/settings.py
+++ page_art/page_art/settings.py
@@ -70,9 +70,10 @@ WSGI_APPLICATION = 'page_art.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'actividades.db',
     }
 }
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
