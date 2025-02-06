43f7d7c33af55502301f0e109bdd8da02ff88b4d
Elvis Rodrigo  Condori Vasquez <ec7902812@gmail.com>
2025-02-06 12:25:05 -0400

    backend

--- Dashboard/pages/services/excelFileService.ts
+++ Dashboard/pages/services/excelFileService.ts
@@ -1,6 +1,6 @@
 import { ExcelFile } from "../models/ExcelFile";
 
-const API_URL = "http://0.0.0.0:8000/excel-files/";
+const API_URL = "http://backend:8000/api/excel-files/";
 
 // Obtener todos los archivos Excel
 export const getExcelFiles = async (): Promise<ExcelFile[]> => {
